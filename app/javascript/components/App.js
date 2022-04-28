3beefd331d079bc88e16f8df364f24b6f25cc07d
Amkam <amaduskamara@gmail.com>
2022-04-28 10:11:23 +0000

    Fetch random greeting in greeting component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Greeting from "./Greeting";
 import Home from "./Home";
+import store from "../redux/configureStore";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/greetings" element={<Greeting />} />
-          </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/greetings" element={<Greeting />} />
+            </Routes>
+          </BrowserRouter>
+        </Provider>
       </>
     );
   }
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,13 +1,28 @@
-import React from "react"
-import PropTypes from "prop-types"
-class Greeting extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <h1>Hello Greeting Text</h1>
-      </React.Fragment>
-    );
-  }
-}
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchGreetingApi } from "../redux/greetings/greetings";
 
-export default Greeting
+const Greeting = () => {
+  const greeting = useSelector((state) => state.greetingsReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchGreetingApi());
+  }, [dispatch]);
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+  return (
+    <div className="mt-20 flex flex-col items-center">
+      <p className=" font-bold">{greeting}</p>
+      <button
+        onClick={refreshPage}
+        className="bg-sky-500 p-2 mt-4 rounded text-slate-50"
+      >
+        Get another greeting
+      </button>
+    </div>
+  );
+};
+
+export default Greeting;
